c0cf7aa6a57b274a131a6785e2d9161b0b7d4116
jgabaut <109908086+jgabaut@users.noreply.github.com>
2024-02-13 00:29:44 +0100

    feat: move animations includes to game_animations.h

--- src/game_curses.h
+++ src/game_curses.h
@@ -41,28 +41,8 @@
 
 #include "palette.h"
 #include "turn_op.h"
+#include "game_animations.h"
 
-#include "./animations/knight_tapis.h"
-#include "./animations/mage_spark.h"
-#include "./animations/archer_drop.h"
-#include "./animations/assassin_poof.h"
-
-#include "./animations/mummy_shuffle.h"
-#include "./animations/ghost_spell.h"
-#include "./animations/boar_scream.h"
-#include "./animations/troll_club.h"
-#include "./animations/goblin_shoot.h"
-#include "./animations/zombie_walk.h"
-#include "./animations/imp_fireball.h"
-#include "./animations/werewolf_transform.h"
-
-#include "./animations/crawlingdude_crawl.h"
-#include "./animations/srwarthog_square.h"
-#include "./animations/headlessninja_throw.h"
-#include "./animations/bluetroll_wonder.h"
-
-#include "./animations/enter_door.h"
-#include "./animations/alt_chest_opening.h"
 
 int display_colorpairs(void);
 void print_encoded_char(WINDOW * w, int y, int x, char c);
--- src/game_rl.h
+++ src/game_rl.h
@@ -23,28 +23,7 @@
 #include "game_utils.h"
 #include "palette.h"
 #include "sprites.h"
-
-#include "./animations/knight_tapis.h"
-#include "./animations/mage_spark.h"
-#include "./animations/archer_drop.h"
-#include "./animations/assassin_poof.h"
-
-#include "./animations/mummy_shuffle.h"
-#include "./animations/ghost_spell.h"
-#include "./animations/boar_scream.h"
-#include "./animations/troll_club.h"
-#include "./animations/goblin_shoot.h"
-#include "./animations/zombie_walk.h"
-#include "./animations/imp_fireball.h"
-#include "./animations/werewolf_transform.h"
-
-#include "./animations/crawlingdude_crawl.h"
-#include "./animations/srwarthog_square.h"
-#include "./animations/headlessninja_throw.h"
-#include "./animations/bluetroll_wonder.h"
-
-#include "./animations/enter_door.h"
-#include "./animations/alt_chest_opening.h"
+#include "game_animations.h"
 
 #define EXPECTED_RAYLIB_VERSION_MAJOR 4 /**< Defines min expected major ncurses version.*/
 #define EXPECTED_RAYLIB_VERSION_MINOR 5 /**< Defines min expected minor ncurses version.*/
