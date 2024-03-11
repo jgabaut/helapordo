db19adf9898b021fb9813f771a8ef9469f207b73
jgabaut <109908086+jgabaut@users.noreply.github.com>
2024-03-11 07:56:05 +0100

    feat: add more packed structures

--- src/ser/ser_core.h
+++ src/ser/ser_core.h
@@ -43,6 +43,101 @@ typedef struct SerTurncounter {
 #pragma pack(pop)
 #endif
 
+/**
+ * Serialized Perk. Packed struct.
+ * Can be turned into a Perk with deser_Perk().
+ * @see Perk
+ * @see deser_Perk()
+ */
+#ifdef __GNUC__
+typedef struct __attribute__((packed)) SerPerk {
+#else
+#pragma pack(push, 1)
+typedef struct SerPerk {
+#endif
+    perkClass class;	  /**< Defines which kind of Perk the instance relates to*/
+    int32_t innerValue;	/**< Contains an integer for perks that need a state*/
+#ifdef __GNUC__
+} SerPerk;
+#else
+} SerPerk;
+#pragma pack(pop)
+#endif
+
+/**
+ * Serialized Skillslot. Packed struct.
+ * Can be turned into a Skillslot with deser_Skillslot().
+ * @see Skillslot
+ * @see deser_Skillslot()
+ */
+#ifdef __GNUC__
+typedef struct __attribute__((packed)) SerSkillslot {
+#else
+#pragma pack(push, 1)
+typedef struct SerSkillslot {
+#endif
+    skillType class;	  /**< Defines which kind of Skillslot the instance relates to*/
+    int32_t enabled;	/**< Flag defining if the slot is initialised.*/
+#ifdef __GNUC__
+} SerSkillslot;
+#else
+} SerSkillslot;
+#pragma pack(pop)
+#endif
+
+/**
+ * Serialized Equip. Packed struct.
+ * Can be turned into an Equip with deser_Equip().
+ * @see Equip
+ * @see deser_Equip()
+ */
+#ifdef __GNUC__
+typedef struct __attribute__((packed)) SerEquip {
+#else
+#pragma pack(push, 1)
+typedef struct SerEquip {
+#endif
+    equipClass class;	  /**< Defines which kind of Equip the instance relates to*/
+    Equipzone type;	/**< Defines which kind of Equipzone the instance relates to*/
+    int32_t equipped; /**< Flag indicating the instance as currently in use if set.*/
+    int32_t level;
+    int32_t atk;
+    int32_t def;
+    int32_t vel;
+    int32_t enr;
+    int32_t bonus;
+    int32_t perksCount;
+    int32_t cost;
+    quality qual;
+    SerPerk perks[EQUIPPERKSMAX];
+#ifdef __GNUC__
+} SerEquip;
+#else
+} SerEquip;
+#pragma pack(pop)
+#endif
+
+/**
+ * Serialized Equipslot. Packed struct.
+ * Can be turned into an Equipslot with deser_Equipslot().
+ * @see Equipslot
+ * @see deser_Equipslot()
+ */
+#ifdef __GNUC__
+typedef struct __attribute__((packed)) SerEquipslot {
+#else
+#pragma pack(push, 1)
+typedef struct SerEquipslot {
+#endif
+    Equipzone type;	/**< Defines which kind of Equipzone the instance relates to*/
+    int32_t active; /**< Flag indicating the instance as currently in use if set.*/
+#ifdef __GNUC__
+} SerEquipslot;
+#else
+} SerEquipslot;
+#pragma pack(pop)
+#endif
+
 bool appendSerTurncounter(const char* filename, SerTurncounter* data);
 
 bool readSerTurncounter(const char* filename, size_t offset, SerTurncounter* data);
