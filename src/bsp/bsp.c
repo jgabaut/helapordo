6814fdc781c8d406a765b70e4fc147e6a40e1294
jgabaut <109908086+jgabaut@users.noreply.github.com>
2024-04-29 17:27:27 +0200

    chore: add spdx header to bsp.c

--- src/bsp/bsp.c
+++ src/bsp/bsp.c
@@ -1,3 +1,28 @@
+//
+// Directly inspired -basically stolen- from bsp-dungeon-generator by Matúš Ďurica.
+//
+// Reference repo: https://github.com/mtsdurica/bsp-dungeon-generator
+// Original license: MIT.
+// The author of the original code gave permission to re-license it to shit helapordo with just one license.
+//
+// jgabaut @ github.com/jgabaut
+// SPDX-License-Identifier: GPL-3.0-only
+/*
+    Copyright (C) 2022-2024 jgabaut
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "bsp.h"
 
 /**
