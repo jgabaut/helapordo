4d09eef52e44f10946a1679fce4f86c058b1abd9
jgabaut <109908086+jgabaut@users.noreply.github.com>
2024-05-17 10:39:41 +0200

    chore: update year

--- scripts/bootstrap_anvil.sh
+++ scripts/bootstrap_anvil.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #  SPDX-License-Identifier: GPL-3.0-only
 #  Script to link binary for anvil
-#    Copyright (C) 2023  jgabaut
+#    Copyright (C) 2023-2024  jgabaut
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
