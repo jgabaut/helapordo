be2348ec6e3cf55ae159f5029e493daf08607523
jgabaut <109908086+jgabaut@users.noreply.github.com>
2024-05-30 23:38:52 +0200

    feat: collect animations preload in game_animations.c

--- src/core/game_animations.h
+++ src/core/game_animations.h
@@ -1,3 +1,20 @@
+// jgabaut @ github.com/jgabaut
+// SPDX-License-Identifier: GPL-3.0-only
+/*
+    Copyright (C) 2022-2024 jgabaut
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #ifndef GAME_ANIMATIONS_H_
 #define GAME_ANIMATIONS_H_
 #include "../animations/knight_tapis.h"
@@ -22,4 +39,10 @@
 #include "../animations/enter_door.h"
 #include "../animations/alt_chest_opening.h"
 
+#include "../utils/game_utils.h"
+
+void hlpd_load_fighter_animations(char fighter_sprites[CLASSESMAX+1][MAXFRAMES][MAXROWS][MAXCOLS]);
+void hlpd_load_enemy_animations(char enemy_sprites[ENEMYCLASSESMAX+1][MAXFRAMES][MAXROWS][MAXCOLS]);
+void hlpd_load_boss_animations(char boss_sprites[BOSSCLASSESMAX + 1][MAXFRAMES][MAXROWS][MAXCOLS]);
+void hlpd_load_animations(char fighter_sprites[CLASSESMAX+1][MAXFRAMES][MAXROWS][MAXCOLS], char enemy_sprites[ENEMYCLASSESMAX+1][MAXFRAMES][MAXROWS][MAXCOLS], char boss_sprites[BOSSCLASSESMAX + 1][MAXFRAMES][MAXROWS][MAXCOLS]);
 #endif // GAME_ANIMATIONS_H_
