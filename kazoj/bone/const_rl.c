68909e084d63e256da78f999ee0149ca58f09f09
jgabaut <109908086+jgabaut@users.noreply.github.com>
2024-10-24 07:03:12 +0200

    feat: bump amboso, invil

--- kazoj/bone/const_rl.c
+++ kazoj/bone/const_rl.c
@@ -11,7 +11,7 @@ void fail(char* msg, int ex, int val) {
 
 int main(void) {
  int check = -1;
- if ( ! (( check = strcmp(VERSION, "1.4.11") ) == 0)) {
+ if ( ! (( check = strcmp(VERSION, "1.4.12") ) == 0)) {
    fail("VERSION",0,check);
  };
  if ( ! (HLP_MAX_INDEX == 35) ) {
